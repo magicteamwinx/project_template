4f380fb80fe26243554bcc262081249a6950aa51
Anastasiia <nastya.lavrishin@gmail.com>
2026-03-30 22:32:16 +0300

    Completing the task

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    pass
+from app.io.input import input_from_console, read_with_builtin
+from app.io.output import output_to_console, write_with_builtin
 
-if __name__ == "main":
+def main():
+    # 1. Ввід з консолі
+    text = input_from_console()
+    output_to_console(f"Ви ввели: {text}")
+    write_with_builtin("data/output.txt", text)
+
+if __name__ == "__main__":
     main()
